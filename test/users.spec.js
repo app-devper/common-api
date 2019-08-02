42ff2b1f896f061b07495842bc5c12c056fa4af0
worawit <worawit.bs@gmail.com>
2019-08-02 15:34:50 +0700

    update test script

--- test/users.spec.js
+++ test/users.spec.js
@@ -6,156 +6,154 @@ import server from '../app/server'
 const { expect } = chai;
 const request = supertest.agent(server);
 
-describe('Routing /users', () => {
-  describe('#User Service', () => {
-    let data;
-    let user;
-    it('login', (done) => {
-      request.post('/api/auth')
-        .send({ username: 'wowit', pwd: '5f4dcc3b5aa765d61d8327deb882cf99', channel: 'app' })
-        .end((err, res) => {
-          if (err) done(err);
-          else {
-            expect(res.statusCode).to.eql(200);
-            data = res.body.data;
-            done();
-          }
-        });
-    }).timeout(10000);
+describe('#User Service', () => {
+  let data;
+  let user;
+  it('login', (done) => {
+    request.post('/api/auth')
+      .send({ username: 'wowit', pwd: '5f4dcc3b5aa765d61d8327deb882cf99', channel: 'app' })
+      .end((err, res) => {
+        if (err) done(err);
+        else {
+          expect(res.statusCode).to.eql(200);
+          data = res.body.data;
+          done();
+        }
+      });
+  }).timeout(10000);
 
-    it('get user id', (done) => {
-      request.get('/api/users/' + data.user._id)
-        .set('x-access-token', data.accessToken)
-        .set('dc-user-id', data.user._id)
-        .end((err, res) => {
-          if (err) done(err);
-          else {
-            expect(res.statusCode).to.eql(200);
-            done();
-          }
-        });
-    }).timeout(10000);
+  it('get user id', (done) => {
+    request.get('/api/users/' + data.user._id)
+      .set('x-access-token', data.accessToken)
+      .set('dc-user-id', data.user._id)
+      .end((err, res) => {
+        if (err) done(err);
+        else {
+          expect(res.statusCode).to.eql(200);
+          done();
+        }
+      });
+  }).timeout(10000);
 
-    it('get users', (done) => {
-      request.get('/api/users')
-        .set('x-access-token', data.accessToken)
-        .set('dc-user-id', data.user._id)
-        .end((err, res) => {
-          if (err) done(err);
-          else {
-            expect(res.statusCode).to.eql(200);
-            done();
-          }
-        });
-    }).timeout(10000);
+  it('get users', (done) => {
+    request.get('/api/users')
+      .set('x-access-token', data.accessToken)
+      .set('dc-user-id', data.user._id)
+      .end((err, res) => {
+        if (err) done(err);
+        else {
+          expect(res.statusCode).to.eql(200);
+          done();
+        }
+      });
+  }).timeout(10000);
 
-    it('add user duplicate', (done) => {
-      request.post('/api/users')
-        .set('x-access-token', data.accessToken)
-        .set('dc-user-id', data.user._id)
-        .send({
-          firstName: data.user.firstName,
-          lastName: data.user.lastName,
-          email: data.user.email,
-          phone: data.user.phone,
-          password: '5f4dcc3b5aa765d61d8327deb882cf99',
-          status: data.user.user,
-          username: data.user.username,
-          role: data.user.role
-        })
-        .end((err, res) => {
-          if (err) done(err);
-          else {
-            expect(res.statusCode).to.eql(409);
-            done();
-          }
-        });
-    }).timeout(10000);
+  it('add user duplicate', (done) => {
+    request.post('/api/users')
+      .set('x-access-token', data.accessToken)
+      .set('dc-user-id', data.user._id)
+      .send({
+        firstName: data.user.firstName,
+        lastName: data.user.lastName,
+        email: data.user.email,
+        phone: data.user.phone,
+        password: '5f4dcc3b5aa765d61d8327deb882cf99',
+        status: data.user.user,
+        username: data.user.username,
+        role: data.user.role
+      })
+      .end((err, res) => {
+        if (err) done(err);
+        else {
+          expect(res.statusCode).to.eql(409);
+          done();
+        }
+      });
+  }).timeout(10000);
 
-    it('add user', (done) => {
-      request.post('/api/users')
-        .set('x-access-token', data.accessToken)
-        .set('dc-user-id', data.user._id)
-        .send({
-          firstName: data.user.firstName,
-          lastName: data.user.lastName,
-          email: data.user.email,
-          phone: data.user.phone,
-          password: '5f4dcc3b5aa765d61d8327deb882cf99',
-          status: data.user.user,
-          username: 'mocha',
-          role: data.user.role
-        })
-        .end((err, res) => {
-          if (err) done(err);
-          else {
-            expect(res.statusCode).to.eql(200);
-            user = res.body.data;
-            done();
-          }
-        });
-    }).timeout(10000);
+  it('add user', (done) => {
+    request.post('/api/users')
+      .set('x-access-token', data.accessToken)
+      .set('dc-user-id', data.user._id)
+      .send({
+        firstName: data.user.firstName,
+        lastName: data.user.lastName,
+        email: data.user.email,
+        phone: data.user.phone,
+        password: '5f4dcc3b5aa765d61d8327deb882cf99',
+        status: data.user.user,
+        username: 'mocha',
+        role: data.user.role
+      })
+      .end((err, res) => {
+        if (err) done(err);
+        else {
+          expect(res.statusCode).to.eql(200);
+          user = res.body.data;
+          done();
+        }
+      });
+  }).timeout(10000);
 
-    it('update user', (done) => {
-      request.put('/api/users/' + user._id)
-        .set('x-access-token', data.accessToken)
-        .set('dc-user-id', data.user._id)
-        .send({
-          firstName: data.user.firstName,
-          lastName: data.user.lastName,
-          email: data.user.email,
-          phone: data.user.phone,
-          status: data.user.user,
-          username: 'mocha',
-          role: data.user.role
-        })
-        .end((err, res) => {
-          if (err) done(err);
-          else {
-            expect(res.statusCode).to.eql(200);
-            user = res.body.data;
-            done();
-          }
-        });
-    }).timeout(10000);
+  it('update user', (done) => {
+    request.put('/api/users/' + user._id)
+      .set('x-access-token', data.accessToken)
+      .set('dc-user-id', data.user._id)
+      .send({
+        firstName: data.user.firstName,
+        lastName: data.user.lastName,
+        email: data.user.email,
+        phone: data.user.phone,
+        status: data.user.user,
+        username: 'mocha',
+        role: data.user.role
+      })
+      .end((err, res) => {
+        if (err) done(err);
+        else {
+          expect(res.statusCode).to.eql(200);
+          user = res.body.data;
+          done();
+        }
+      });
+  }).timeout(10000);
 
-    it('remove user', (done) => {
-      request.del('/api/users/' + user._id)
-        .set('x-access-token', data.accessToken)
-        .set('dc-user-id', data.user._id)
-        .end((err, res) => {
-          if (err) done(err);
-          else {
-            expect(res.statusCode).to.eql(200);
-            user = res.body.data;
-            done();
-          }
-        });
-    }).timeout(10000);
+  it('remove user', (done) => {
+    request.del('/api/users/' + user._id)
+      .set('x-access-token', data.accessToken)
+      .set('dc-user-id', data.user._id)
+      .end((err, res) => {
+        if (err) done(err);
+        else {
+          expect(res.statusCode).to.eql(200);
+          user = res.body.data;
+          done();
+        }
+      });
+  }).timeout(10000);
 
-    it('logout', (done) => {
-      request.get('/api/auth/logout')
-        .set('x-access-token', data.accessToken)
-        .set('dc-user-id', data.user._id)
-        .end((err, res) => {
-          if (err) done(err);
-          else {
-            expect(res.statusCode).to.eql(200);
-            done();
-          }
-        });
-    }).timeout(10000);
+  it('logout', (done) => {
+    request.get('/api/auth/logout')
+      .set('x-access-token', data.accessToken)
+      .set('dc-user-id', data.user._id)
+      .end((err, res) => {
+        if (err) done(err);
+        else {
+          expect(res.statusCode).to.eql(200);
+          done();
+        }
+      });
+  }).timeout(10000);
 
-    it('get user invalid', (done) => {
-      request.get('/api/users/' + data.user._id)
-        .set('x-access-token', data.accessToken)
-        .end((err, res) => {
-          if (err) done(err);
-          else {
-            expect(res.statusCode).to.eql(401);
-            done();
-          }
-        });
-    }).timeout(10000);
-  });
+  it('get user invalid', (done) => {
+    request.get('/api/users/' + data.user._id)
+      .set('x-access-token', data.accessToken)
+      .end((err, res) => {
+        if (err) done(err);
+        else {
+          expect(res.statusCode).to.eql(401);
+          done();
+        }
+      });
+  }).timeout(10000);
 });
