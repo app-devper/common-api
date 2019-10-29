23c0969fcab9c4a9d08d3babf30d23bf4da92dd5
worawit <worawit.bs@gmail.com>
2019-10-29 20:30:14 +0700

    App - Edit response register device

--- app/presentation/routes/device.api.js
+++ app/presentation/routes/device.api.js
@@ -2,16 +2,16 @@ import { POST, route } from 'awilix-express'
 
 @route('/device')
 export default class DeviceApi {
-  constructor ({ addDeviceUseCase }) {
+  constructor({ addDeviceUseCase }) {
     this.addDeviceUseCase = addDeviceUseCase;
   }
 
   @POST()
-  async addDevice (req, res, next) {
+  async addDevice(req, res, next) {
     try {
       const body = req.body;
       const result = await this.addDeviceUseCase.execute(body);
-      res.status(200).send(result)
+      res.status(200).send({ publicKey: result._id })
     } catch (err) {
       next(err)
     }
